9ecbcd4f8ba773003b0c7bf1f014a82c014b4293
Huttner David <huttnerd@gmail.com>
2023-06-29 14:37:38 +0200

    The game is playable

--- client/src/views/Play.jsx
+++ client/src/views/Play.jsx
@@ -1,19 +1,56 @@
 import React, { useState } from "react";
-import Question from "../components/Question"
+import Question from "../components/Question";
 
 function Play() {
   const [difficulty, setDifficulty] = useState("easy");
   const [pickedDiff, setPick] = useState(false);
+  const [questions, setQuestions] = useState([
+    {
+      question: "sampkle",
+      correct_answer: "sample",
+      incorrect_answers: ["test", "test", "test"],
+    },
+  ]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [finishedGame, setFinish] = useState(false);
 
   const difficultyPicked = async function (diff) {
     setDifficulty(diff);
     setPick(true);
+    fetch(
+      `https://opentdb.com/api.php?amount=10&difficulty=${diff}&type=multiple`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestions(data["results"]);
+      });
+  };
+
+  const handleAnswer = function (answer) {
+    if (currentQuestion === 9) {
+      setFinish(true);
+      if (answer) {
+        setPlayerScore(playerScore + 1);
+      }
+    } else if (answer) {
+      setPlayerScore(playerScore + 1);
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+    }
   };
 
   return (
     <div>
-      {pickedDiff ? (
-        <Question />
+      {finishedGame ? (
+        <div>You got {playerScore} out of 10 right</div>
+      ) : pickedDiff ? (
+        <Question
+          question={questions[currentQuestion]}
+          handler={handleAnswer}
+          score={playerScore}
+        />
       ) : (
         <div>
           <h1>Pick a difficulty</h1>
